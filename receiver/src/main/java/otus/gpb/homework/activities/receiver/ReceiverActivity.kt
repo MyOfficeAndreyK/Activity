cdc33acc9cc2b6a9020c581efe29c6db6bf50fe3
AndreyKharkovsky <andrey.kharkovsky@myoffice.team>
2023-02-04 16:37:00 +0300

    Activity 2 homework

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,6 +1,9 @@
 package otus.gpb.homework.activities.receiver
 
+import androidx.core.content.ContextCompat
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
@@ -8,5 +11,14 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val title = intent.getStringExtra("title")
+        val year = intent.getStringExtra("year")
+        val description = intent.getStringExtra("description")
+        findViewById<TextView>(R.id.titleTextView).text = title
+        findViewById<TextView>(R.id.descriptionTextView).text = description
+        findViewById<TextView>(R.id.yearTextView).text = year
+
+        findViewById<ImageView>(R.id.posterImageView).setImageDrawable(ContextCompat.getDrawable(this, R.drawable.interstellar))
     }
 }
